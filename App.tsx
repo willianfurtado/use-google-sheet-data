40a1aa9aefec2bd73645ff1492a296abb5dabf19
willianfurtado <willianjj15@hotmail.com>
2025-02-25 10:52:12 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from "react";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default function useGoogleSheetData(apiKey: string, spreadSheetId: string, range: string) {
+  const [data, setData] = useState<string[][]>([]);
+  const [error, setError] = useState<string | null>(null);
+  
+  useEffect(() => {
+    
+    const loadPrice = async () => {
+      setError(null);
+
+      const url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadSheetId}/values/${range}?key=${apiKey}`;
+
+      try {
+        const response = await fetch(url);
+
+        if (!response.ok) {
+          throw new Error(`Erro HTTP! Código: ${response.status}`);
+        }
+
+        const dataPrice = await response.json();
+        setData(dataPrice.values || []);
+      } catch (error: any) {
+        setError(error.message);
+      } 
+    };
+
+    loadPrice(); 
+  }, [apiKey, spreadSheetId, range]);
+
+  return { data, error };
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
